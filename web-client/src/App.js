8237778634bd20690bcbf77e14e1c4480aa5aa3d
binys21 <binys1121@ewhain.net>
2023-08-25 22:44:32 +0900

    npm test

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
 import { Routes } from "react-router-dom";
-import { LoginInfo } from "../context/LoginUseContext";
+// import { LoginInfo } from "../context/LoginUseContext";
 
 function App() {
   const [loggedInUser, setLoggedInUser] = useState(""); // 로그인 상태와 상태 변경 함수 선언 (전역변수 같은거라고 생각하면 됨)
 
   return (
     <div className="App">
-      <LoginInfo.Provider value={{ loggedInUser, setLoggedInUser }}>
-        <Routes>
+
+      {/* <LoginInfo.Provider value={{ loggedInUser, setLoggedInUser }}> */}
+        {/* <Routes> */}
           {/*<Route path="/mapHome" element={<MapHomepage />}></Route>*/}
-        </Routes>
-      </LoginInfo.Provider>
+        {/* </Routes> */}
+      {/* </LoginInfo.Provider> */}
+
     </div>
   );
 }
--- web-client/src/components/Study.js
+++ web-client/src/components/Study.js
@@ -4,6 +4,16 @@ import styled from "styled-components";
 const Study=()=>{
     return(
         <>
+        <Wrapper>
+            <TitleWrapper></TitleWrapper>
+        </Wrapper>
         </>
     );
 };
+
+export default Study;
+const Wrapper=styled.div`
+`;
+
+const TitleWrapper=styled.div`
+`;
