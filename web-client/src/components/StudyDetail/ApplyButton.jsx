2033328297293c9344f2cb0faf6fc2479493e033
binys21 <binys1121@ewhain.net>
2023-08-26 05:36:02 +0900

    [styled] createstudy 수정중

--- web-client/src/components/StudyDetail/ApplyButton.jsx
+++ web-client/src/components/StudyDetail/ApplyButton.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 function ApplyButton() {
-    return <Button>바로 지원하기</Button>;
+  const navigate = useNavigate();
+  const gotoapply = () => {
+    navigate("/createstudy");
+  };
+  return <Button onClick={gotoapply}>바로 지원하기</Button>;
 }
 
 const Button = styled.button`
-    display: block;
-    color: white;
-    font-size: 14px;
-    font-weight: 700;
-    margin: auto;
-    width: 90%;
-    height: 50px;
-    background-color: #003378;
-    border: none;
-    border-radius: 10px;
+  display: block;
+  color: white;
+  font-size: 14px;
+  font-weight: 700;
+  margin: auto;
+  width: 90%;
+  height: 50px;
+  background-color: #003378;
+  border: none;
+  border-radius: 10px;
 
-    position: fixed;
-    left: 50%;
-    transform: translateX(-50%);
-    max-width: 460px;
-    bottom: 20px;
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  max-width: 460px;
+  bottom: 20px;
 `;
 export default ApplyButton;
