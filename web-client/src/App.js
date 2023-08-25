1249b167fefc23734e695c2e755e8665fc65a6fd
jisooshin <kelly6226@naver.com>
2023-08-25 22:06:32 +0900

    [FEAT] 로그인 관련 context 추가 & Route 라이브러리 추가

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,5 +1,19 @@
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { LoginInfo } from "./context/LoginUseContext";
+
 function App() {
-  return <div className="App"></div>;
+  const [loggedInUser, setLoggedInUser] = useState(""); // 로그인 상태와 상태 변경 함수 선언 (전역변수 같은거라고 생각하면 됨)
+
+  return (
+    <div className="App">
+      <LoginInfo.Provider value={{ loggedInUser, setLoggedInUser }}>
+        <Routes>
+          {/*<Route path="/mapHome" element={<MapHomepage />}></Route>*/}
+        </Routes>
+      </LoginInfo.Provider>
+    </div>
+  );
 }
 
 export default App;
